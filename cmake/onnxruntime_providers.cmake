442fa4ee686b64533fc95f472e916dd48cec4acb
MaajidKhan <n.maajidkhan@gmail.com>
2022-03-18 17:39:20 +0530

    CMAKE

--- cmake/onnxruntime_providers.cmake
+++ cmake/onnxruntime_providers.cmake
@@ -725,8 +725,8 @@ if (onnxruntime_USE_OPENVINO)
   endif()
 
   # Header paths
-  # find_package(InferenceEngine REQUIRED)
-  # find_package(ngraph REQUIRED)
+  #find_package(InferenceEngine REQUIRED)
+  #find_package(ngraph REQUIRED)
 
   if (WIN32)
     unset(CMAKE_MAP_IMPORTED_CONFIG_RELWITHDEBINFO)
@@ -736,7 +736,7 @@ if (onnxruntime_USE_OPENVINO)
     add_definitions(-DIO_BUFFER_ENABLED=1)
     list(APPEND OPENVINO_LIB_LIST $ENV{OPENCL_LIBS} ${InferenceEngine_LIBRARIES} ${NGRAPH_LIBRARIES} ngraph::onnx_importer ${PYTHON_LIBRARIES})
   else()
-    list(APPEND OPENVINO_LIB_LIST ${InferenceEngine_LIBRARIES} ${NGRAPH_LIBRARIES} ${PYTHON_LIBRARIES})
+    list(APPEND OPENVINO_LIB_LIST $ENV{INTEL_OPENVINO_DIR} ${InferenceEngine_LIBRARIES} ${NGRAPH_LIBRARIES} ${PYTHON_LIBRARIES})
   endif()
   
   include_directories(${ONNXRUNTIME_ROOT}/../runtime/include/ie/ ${ONNXRUNTIME_ROOT}/../runtime/include/ ${ONNXRUNTIME_ROOT}/../runtime/include/openvino)
@@ -751,7 +751,7 @@ if (onnxruntime_USE_OPENVINO)
   endif()
   add_dependencies(onnxruntime_providers_openvino onnxruntime_providers_shared ${onnxruntime_EXTERNAL_DEPENDENCIES})
   target_include_directories(onnxruntime_providers_openvino SYSTEM PUBLIC ${ONNXRUNTIME_ROOT} ${CMAKE_CURRENT_BINARY_DIR} ${eigen_INCLUDE_DIRS} ${OpenVINO_INCLUDE_DIR} ${OPENVINO_INCLUDE_DIR_LIST} ${PYTHON_INCLUDE_DIRS} $ENV{OPENCL_INCS})
-  target_link_libraries(onnxruntime_providers_openvino ${ONNXRUNTIME_PROVIDERS_SHARED} ${INTEL_OPENVINO_DIR} absl::raw_hash_set absl::hash)
+  target_link_libraries(onnxruntime_providers_openvino ${ONNXRUNTIME_PROVIDERS_SHARED} ${OPENVINO_LIB_LIST} absl::raw_hash_set absl::hash)
 
   if(MSVC)
     target_compile_options(onnxruntime_providers_openvino PUBLIC /wd4099 /wd4275 /wd4100 /wd4005 /wd4244 /wd4267)
