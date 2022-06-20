63608402b6010b47c0be507b2f8b947999a3ae09
mohsin <mohsinx.mohammad@intel.com>
2022-06-20 16:25:34 +0530

    update backend_manager.cc

--- onnxruntime/core/providers/openvino/backend_manager.cc
+++ onnxruntime/core/providers/openvino/backend_manager.cc
@@ -1,11 +1,10 @@
 // Copyright (C) 2019-2022 Intel Corporation
 // Licensed under the MIT License
-
+#include "backend_manager.h"
 #include "core/providers/shared_library/provider_api.h"
 #include <inference_engine.hpp>
 #include "contexts.h"
 
-#include "backend_manager.h"
 #include <fstream>
 #include <vector>
 #include <string>
